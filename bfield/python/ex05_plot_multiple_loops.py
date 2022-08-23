9431621f52486bbcf2aea9bee92a94c000dc9895
Toyo <ty20@illinois.edu>
2022-08-22 23:19:50 -0500

    The pile of coils can be rotated.

--- bfield/python/ex05_plot_multiple_loops.py
+++ bfield/python/ex05_plot_multiple_loops.py
@@ -18,7 +18,7 @@ current_time = now.strftime("%Y-%m-%d %H:%M:%S %p")
 print(current_time)
 
 # Loops ( Ra,I0,Nturns, Xcenter,Ycenter,Zcenter, EulerAngles1,2,3 )
-EA1 = 30
+EA1 = 45
 Loops = np.array([[ 0.05,100,1,  0.04*np.sin(EA1*np.pi/180.0), 0.04*np.cos(EA1*np.pi/180.0),0, EA1,0,0 ],
                   [ 0.05,100,1,  0.03*np.sin(EA1*np.pi/180.0), 0.03*np.cos(EA1*np.pi/180.0),0, EA1,0,0 ],
                   [ 0.05,100,1,  0.02*np.sin(EA1*np.pi/180.0), 0.02*np.cos(EA1*np.pi/180.0),0, EA1,0,0 ],
@@ -30,8 +30,9 @@ Loops = np.array([[ 0.05,100,1,  0.04*np.sin(EA1*np.pi/180.0), 0.04*np.cos(EA1*n
                   [ 0.05,100,1, -0.04*np.sin(EA1*np.pi/180.0),-0.04*np.cos(EA1*np.pi/180.0),0, EA1,0,0 ] ])
 Nloops = np.size(Loops,0)
 
-X = np.linspace( -0.1, 0.1, 200 )
-Y = np.linspace( -0.1, 0.1, 200 )
+mesh_size = 500
+X = np.linspace( -0.1, 0.1, mesh_size )
+Y = np.linspace( -0.1, 0.1, mesh_size )
 Bnorm = np.zeros((X.size,Y.size))
 
 for i in range(0,X.size):
@@ -48,11 +49,11 @@ for i in range(0,X.size):
 
 plt.figure(1)
 XX,YY = np.meshgrid(X,Y)
-#plt.contourf(np.transpose(XX),np.transpose(YY),Bnorm,np.linspace(0,0.06,30))
-plt.contourf(np.transpose(XX),np.transpose(YY),Bnorm,30)
+#plt.contourf(np.transpose(XX),np.transpose(YY),Bnorm,30)
+plt.contourf(np.transpose(XX),np.transpose(YY),Bnorm,np.linspace(0,0.032,30))
 plt.colorbar()
 plt.xlabel('X [m]')
 plt.ylabel('Y [m]')
-plt.title('B-field magnitude [T] - Multiple Loops \n (run by Toyo at '+current_time+')')
+plt.title('B-field magnitude [T] - Multiple Loops\n EA = {:.0f} deg, mesh size = ({:.0f}x{:.0f})\n (run by Toyo at '.format(EA1,mesh_size,mesh_size)+current_time+')')
 plt.savefig('ex05_plot_multiple_loops (run by Toyo at '+current_time+').png',dpi=150)
 plt.show()
