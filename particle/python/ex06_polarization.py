e19fc9b6e0c315f8a4df9c307c9a25cf6c0b7c7a
Toyo <ty20@illinois.edu>
2022-08-31 10:35:07 -0500

    Added #% and pathname to /ode/python/

--- particle/python/ex06_polarization.py
+++ particle/python/ex06_polarization.py
@@ -1,9 +1,10 @@
+#%%
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-sys.path.insert(1, '../../ode/python/')
+sys.path.insert(1, '/Users/toyo/Library/CloudStorage/GoogleDrive-ty20@illinois.edu/My Drive/NPRE598 Computational Plasma Physics/PlasmaEDU/ode/python/')
 
 import ode
 
