015bb45e9a904aa373c12fbfaa15cd6ab1f03219
Toyo <ty20@illinois.edu>
2022-09-12 02:14:36 -0500

    to import rk4_2nd

--- HW3_exercise2.py
+++ HW3_exercise2.py
@@ -6,24 +6,29 @@ import matplotlib.pyplot as plt
 sys.path.insert(1, '/Users/toyo/Library/CloudStorage/GoogleDrive-ty20@illinois.edu/My Drive/NPRE598 Computational Plasma Physics/PlasmaEDU/ode/python/')
 import ode
 
-Ωt = np.linspace(0,2*2*np.pi,101)
+Omega_t = np.linspace(0,2*2*np.pi,18)
 two_pi = 2.0*np.pi
 
-Ω = 1
-def harmonic_oscillator(x,Ω):
-    x_dot_dot = Ω**2*x
-    return x_dot_dot
-x_ana = np.cos(Ωt)
+def f(t,x,v):
+    x_dot = v
+    return x_dot
+def g(t,x,v):
+    v_dot = -x
+    return v_dot
+
+x_ana = np.cos(Omega_t)
 x_ana = np.reshape(x_ana,(len(x_ana),1))
-x_rk4 = ode.rk4(harmonic_oscillator,Ωt,np.array([0]))
+x_rk4 = ode.rk4_2nd(f,g,Omega_t,np.array([1.0,0.0]))
+#x_rk4 = ode.rk4(fun,x_dot_rk4,np.array([0.0]))
 
 plt.figure(figsize=(5.5,3.8))
-plt.plot(Ωt/two_pi,x_ana,'k-',label='Analytical Solution')
-plt.plot(Ωt/two_pi,x_rk4,'ro',label='Runge-Kutta (4th)')
+plt.plot(np.linspace(0,2*2*np.pi,101)/two_pi,np.cos(np.linspace(0,2*2*np.pi,101)),'k-',label='Analytical Solution')
+#plt.plot(Omega_t/two_pi,x_ana,'ko',label='Analytical Solution')
+plt.plot(Omega_t/two_pi,x_rk4,'ro',label='Runge-Kutta (4th)')
 plt.xlim([0,2.0])
 plt.xticks(np.arange(0,2.0+0.5,0.5))
 plt.xlabel('time, tΩ/2π')
-plt.ylim([-1,1])
+#plt.ylim([-1,1])
 plt.ylabel('position, x')
 plt.title('Harmonic Oscillator')
 plt.legend(loc='best',framealpha=1)
@@ -32,7 +37,7 @@ plt.tight_layout()
 plt.savefig('HW3_exercise2_plot.png',dpi=150)
 
 plt.figure(figsize=(5.5,3.8))
-plt.plot(Ωt/two_pi,ode.error_absolute(x_ana,x_rk4),'r-',label='Runge-Kutta (4th)')
+plt.plot(Omega_t/two_pi,ode.error_absolute(x_ana,x_rk4),'r-',label='Runge-Kutta (4th)')
 plt.xlim([0,2.0])
 plt.xticks(np.arange(0,2.0+0.5,0.5))
 plt.xlabel('time, tΩ/2π')
